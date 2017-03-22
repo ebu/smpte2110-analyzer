0ba26154c588d06e1fb98a0ec5070070b8c43fe0
Willem Vermost <wvermost74@gmail.com>
2017-03-22 14:28:54 +0100

    Refactored code

--- cfull_analysis.py
+++ cfull_analysis.py
@@ -1,76 +1,51 @@
-#!/usr/bin/python3
-
-import sys
-
-if sys.version_info < (3, 0):
-    print("needs python 3")
-    sys.exit(2)
-
 import getopt
 import pyshark
-import time
 import numpy
 import math
 import sys
 from decimal import *
-from collections import Counter
+
+RTP_CLOCK = 90000  # RTP clock Frequency is defined at 90kHz
+B = 1.1  # Drain factor as defined in SMPTE2110-21
 
 
-def main(argv):
-    try:
-        opts, args = getopt.getopt(argv, "hc:g:", ["help", "cap=", "group="])
-        if not opts:
-            print("No options supplied")
-            usage()
-            sys.exit(2)
-    except getopt.GetoptError:
-        print("Error in options {}".format(opts))
-        usage()
-        sys.exit(2)
+def frame_len(capture):
+    # To calculate Npackets, you need to count the amount of packets between two rtp.marker == 1 flags.
+    # This is as easy as looking to 2 rtp.marker == 1 packets and substract the rtp.sequence number.
+    # The exception that might occurs is that the packet sequence number rotates.
 
-    global capfile
-    global group
+    first_frame = None
+    for pkt in capture:
+        if pkt.rtp.marker == '1':
+            if not first_frame:
+                first_frame = int(pkt.rtp.seq)
+            else:
+                return int(pkt.rtp.seq) - first_frame
+    return None
 
-    for opt, arg in opts:
-        if opt in ("-h", "--help"):
-            usage()
-            sys.exit()
-        elif opt in ("-c", "--cap"):
-            capfile = arg
-        elif opt in ("-g", "--group"):
-            group = arg
-        else:
-            print("unknown option " + opt)
-            usage()
-            sys.exit()
-    decode_str = "udp.port=" + "50000"
-    #decode_str = "udp.port=" + "5000"
 
-    # filtering capture with marker to find nb packets per field
-    global capture_marker
-    idx = 0
-    oldIdx = 0
-    sequencenumber = []
+def frame_rate(capture):
+    # To calculate the framerate of a given capture, you need to look at three consequent rtp time stamps [(t2-t1) +
+    # (t3-t2)] / 2 will result in the average timestamp difference. Note:  the frame periods (difference between 90
+    # kHz timestamps) might not appear constant For example 60/1.001 Hz frame periods effectively alternate between
+    # increments of 1501 and 1502 ticks of the 90 kHz clock.
+    rtp_timestamp = []
 
-    capture_marker = pyshark.FileCapture(capfile, keep_packets=False, decode_as={decode_str: 'rtp'},
-                                         display_filter='ip.dst==' + group + '&& rtp.marker == 1')
+    for pkt in capture:
+        if pkt.rtp.marker == '1':
+            if len(rtp_timestamp) < 3:
+                rtp_timestamp.append(int(pkt.rtp.timestamp))
+            else:
+                frame_rate_c = Decimal(RTP_CLOCK /
+                                       (((rtp_timestamp[2] - rtp_timestamp[1]) + (
+                                           rtp_timestamp[1] - rtp_timestamp[0])) / 2))
+                return frame_rate_c
+    return None
 
-    for pkt in capture_marker:
-        sequencenumber.append(pkt.rtp.seq)
 
-    print("Frames: ", len(sequencenumber))
-
-    # -- Should protect next fomula: sequence numbers will overflow --
-    # ----------------------------------------------------------------
-    npackets = int(sequencenumber[-1]) + 0 - int(sequencenumber[-2])
-    # ----------------------------------------------------------------
-    print("Npackets = {}".format(npackets))
-
-    # analysing all packets
-    capture = pyshark.FileCapture(capfile, keep_packets=False, decode_as={decode_str: 'rtp'},
-                                  display_filter='ip.dst==' + group)
-
+def cfull_analysis(capture, tframe, npackets, B):
     # Time different between packets
+    global idx
     timediffs = []
 
     # SMPTE2110-21: leaky bucket model
@@ -79,20 +54,11 @@ def main(argv):
     # Amount of packets cleared out of the buffer
     cleared = []
 
-    # Number of cleared of packets
-    clearnbr = 0
-
     # Start of the experiment
     initialtime = time = oldTime = 0.000000000
 
-    # Frame duration
-    tframe = float(1 / 59.94)
-
-    # Drain factor as defined in SMPTE2110-21
-    B = 1.1
-
     # Formula defined in SMPTE2110-21
-    tdrain = tframe / npackets / B
+    tdrain = tframe / npackets / Decimal(B)
     print("Tdrain = {}".format(tdrain))
 
     try:
@@ -108,23 +74,22 @@ def main(argv):
                 cfull[-1] = 1
                 cleared.append(0)
 
-            if (oldTime != 0):
+            if oldTime != 0:
                 timediff = time - oldTime
                 timediffs.append(timediff)
                 clearnbr = math.floor((Decimal(time) - Decimal(initialtime)) / Decimal(tdrain))
                 cleared.append(clearnbr)
                 buffer = cfull[-1] + 1 - (cleared[-1] - cleared[-2])
 
-                if (buffer >= 0):
+                if buffer >= 0:
                     cfull.append(buffer)
-                if (buffer < 0):
+                if buffer < 0:
                     cfull.append(0)
             oldTime = time
 
     except KeyboardInterrupt:
         print("\nInterrupted")
 
-    data = Counter(timediffs)
     print("Intial Time = ", initialtime)
     print("#packets = ", idx + 1)
     print("#cleared = ", numpy.max(cleared))
@@ -136,8 +101,7 @@ def main(argv):
     print("minimim = ", numpy.min(timediffs))
     print("Reference Time  :", Decimal(time) - Decimal(initialtime))
 
-    print("Result in: ",capfile + "_" + ".txt")
-    write_array(capfile + "_" + ".txt", cfull)
+    return cfull
 
 
 def write_array(filename, array):
@@ -147,14 +111,63 @@ def write_array(filename, array):
 
     while idx < len(array):
         text_file.write(str(array[idx]) + "\n")
-        idx = idx + 1
+        idx += 1
 
     text_file.close()
     return 0
 
+
 def usage():
-    print("analyzer.py -c|--cap <capture_file>")
-    print("<max_packets> : use \"-\" for all capture")
+    print("cfull_analysis.py -c|--cap <capture_file> -g|--group <multicast_group> -p|--port <udp_port>")
+
+
+def getarguments(argv):
+    global opts
+    short_opts = 'hc:g:p:'
+    long_opts = ["help", "cap=", "group=", "port="]
+
+    try:
+        opts, args = getopt.getopt(argv, short_opts, long_opts)
+        if not opts:
+            print("No options supplied")
+            usage()
+            sys.exit(2)
+    except getopt.GetoptError:
+        print("Error in options {}".format(opts))
+        usage()
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif opt in ("-c", "--cap"):
+            capfile = arg
+        elif opt in ("-g", "--group"):
+            group = arg
+        elif opt in ("-p", "--port"):
+            port = arg
+        else:
+            print("unknown option " + opt)
+            usage()
+            sys.exit()
+    return (capfile, group, port)
+
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    capfile, group, port = getarguments(sys.argv[1:])
+
+    capture = pyshark.FileCapture(capfile, keep_packets=False, decode_as={"udp.port=" + port: 'rtp'},
+                                  display_filter='ip.dst==' + group + ' && rtp.marker == 1')
+    frame_ln = frame_len(capture)
+    print("Npackets  : ", frame_ln)
+
+    framerate = frame_rate(capture)
+    print("Frame Frequency: ", round(framerate, 2), " Hz")
+    tframe = 1 / framerate
+
+    capture = pyshark.FileCapture(capfile, keep_packets=False, decode_as={"udp.port=" + port: 'rtp'},
+                                  display_filter='ip.dst==' + group)
+    cfull_array = cfull_analysis(capture, tframe, frame_ln, B)
+
+    write_array(capfile + "_cfull_" + ".txt", cfull_array)
